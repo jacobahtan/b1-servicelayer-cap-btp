ace883223e6fd7a2071be1c2264009d87f6e5ec7
Jacob Tan <jacobtan89@gmail.com>
2022-08-30 10:02:22 +0000

    local version

--- README.md
+++ README.md
@@ -1,35 +1,23 @@
 # b1-servicelayer-cap-btp
-Utilise service layer in SAP CAP CDS custom event handler logic. Service layer definition is defined through SAP BTP via the Connectivity > Destination service.
+This branch shows how you can quickly hard code service layer variables and test consumption of the API through CDS remote service.
 
 # Getting Started
+Clone the repo and branch into the local version.
 ```bash
-git clone https://github.com/jacobahtan/b1-servicelayer-cap-btp.git b1-servicelayer-cap-btp
+git clone https://github.com/jacobahtan/b1-servicelayer-cap-btp.git b1-servicelayer-cap-local
 ```
+In this branch, the app will consume service layer API directly in the cat-service.js
 
-1. Create a Destination service instance in your SAP BTP Account.
-- SAP BTP Cockpit > Services > Instances & Subscriptions > Create
-- Destination service > Plan: Lite > Create
-- Create Service Key
-- Copy JSON > Paste in default-env.json (__REPLACE_W_DEST_SERVICE_KEY_CREDENTIALS__)
-
-2. Define your SAP Business One Service Layer in SAP BTP Cockpit.
-- SAP BTP Cockpit > Connectivity > Destination
-- Create Destination
-- Name: B1SL
-- Type: HTTP
-- Description: B1SL
-- URL: http://SERVICE_LAYER_URL:PORT/b1s/v1/
-- Proxy Type: Internet
-- Authentication: BasicAuthentication
-- User: { "CompanyDB": "COMPANY_DB", "UserName": "USERNAME" }
-- Password: PASSWORD
-
-In this app, it will utilise "destination" as the destination service name and B1SL as the destination definition name.
-So, please do not change unless you know how to manage this.
-
-3. Run your CAP App.
 ```bash
-cd b1-servicelayer-cap-btp
+cd b1-servicelayer-cap-local
+git checkout -b local 
+```
+Modify the parameters in cat-service.js to your service layer url & port.
+For simplicity, we will use a non-SSL port (HTTP).
+e.g. http://myservicelayer:50001/b1s/v1/
+
+Once defined, let's run the app.
+```bash
 npm install
 cds run
 ```
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,24 +1,14 @@
 const cds = require("@sap/cds");
-const axios = require('axios');
-const xsenv = require('@sap/xsenv');
-const b1dest = "B1SL";  //  create a simple basic auth destination in your btp cockpit
+const b1slurl = "_REPLACE_W_B1_SL_URL_";    // e.g. http://servicelayer:port/b1s/v1/
+const b1companydb = "_REPLACE_W_B1_SL_COMPANYDB_";
+const b1user = "_REPLACE_W_B1_SL_USER_";
+const b1pass = "_REPLACE_W_B1_SL_PASS_";
 
 module.exports = cds.service.impl(async function () {
     const { Books } = this.entities;
 
-    var b1session, b1slurl, b1credentials;
-
-    /** PLEASE READ FIRST
-     *  Mandatory: Create a destination instance in SAP BTP then create a service key and copy the JSON and paste into the default-env.json.
-     * If not there will be an error of properties undefined.
-    */
-    getDestination(b1dest).then(dest => {
-        b1session = "Basic " + dest.authTokens[0].value;
-        b1slurl = dest.destinationConfiguration.URL;
-        b1credentials = JSON.parse(dest.destinationConfiguration.User);
-        b1credentials.Password = dest.destinationConfiguration.Password;
-
-        //  Retrieve a cookie from B1 through SL Login.
+    this.on("READ", Books, async () => {
+        var ret;
         var request = require("request");
         var options = {
             method: "POST",
@@ -26,31 +16,32 @@ module.exports = cds.service.impl(async function () {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(b1credentials),
+            body: JSON.stringify({
+                CompanyDB: b1companydb,
+                Password: b1pass,
+                UserName: b1user,
+            }),
         };
         request(options, function (error, response) {
             if (error) throw new Error(error);
             var jsonarr = JSON.parse(response.body);
-            b1session = "B1SESSION=" + jsonarr.SessionId + "; ROUTEID=.node2";
+            var cook = "B1SESSION=" + jsonarr.SessionId + "; ROUTEID=.node2";
+            var request = require("request");
+            var options = {
+                method: "GET",
+                url: b1slurl + "Items?$select=ItemCode,ItemName,BarCode&$top=5",
+                headers: {
+                    "Content-Type": "application/json",
+                    Cookie: cook,
+                }
+            };
+            request(options, function (error, response) {
+                if (error) throw new Error(error);
+                var itemsarr = JSON.parse(response.body);
+                ret = itemsarr.value;
+            });
         });
-    });
 
-    this.on("READ", Books, async () => {
-        var ret;
-        var request = require("request");
-        var options = {
-            method: "GET",
-            url: b1slurl + "Items?$select=ItemCode,ItemName,BarCode&$top=5",
-            headers: {
-                "Content-Type": "application/json",
-                Cookie: b1session,
-            }
-        };
-        request(options, function (error, response) {
-            if (error) throw new Error(error);
-            var itemsarr = JSON.parse(response.body);
-            ret = itemsarr.value;
-        });
         await sleep(2000);
         return ret;
     });
@@ -61,44 +52,3 @@ function sleep(ms) {
         setTimeout(resolve, ms);
     });
 }
-
-/** Default Helper function to auth your app getting connected with SAP BTP Destination services and return Destination object. */
-async function getDestination(dest) {
-    try {
-        xsenv.loadEnv();
-        let services = xsenv.getServices({
-            dest: { tag: 'destination' }
-        });
-        try {
-            let options1 = {
-                method: 'POST',
-                url: services.dest.url + '/oauth/token?grant_type=client_credentials',
-                headers: {
-                    Authorization: 'Basic ' + Buffer.from(services.dest.clientid + ':' + services.dest.clientsecret).toString('base64')
-                }
-            };
-            let res1 = await axios(options1);
-            try {
-                options2 = {
-                    method: 'GET',
-                    url: services.dest.uri + '/destination-configuration/v1/destinations/' + dest,
-                    headers: {
-                        Authorization: 'Bearer ' + res1.data.access_token
-                    }
-                };
-                let res2 = await axios(options2);
-                // return res2.data.destinationConfiguration;
-                return res2.data;
-            } catch (err) {
-                console.log(err.stack);
-                return err.message;
-            }
-        } catch (err) {
-            console.log(err.stack);
-            return err.message;
-        }
-    } catch (err) {
-        console.log(err.stack);
-        return err.message;
-    }
-};
